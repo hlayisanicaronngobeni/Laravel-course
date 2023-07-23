64837174c8bd471b6db457fa32ee8744c80cefd4
hlayisanicaronngobeni <hlayisani.caronngobeni@cm.com>
2023-07-23 14:10:13 +0200

    Sample App

---
+++ app/Models/Post.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use JetBrains\PhpStorm\NoReturn;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+
+{
+    public  $title;
+    public  $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+//    /**
+//     * @param $title
+//     * @param $excerpt
+//     * @param $date
+//     * @param $body
+//     */
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+    }
+
+    public static function all()
+    {
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
+
+    }
+    #[NoReturn] public static function find($slug)
+    {
+        // of all the blog posts, find the one with a slug that matches the one that was requested
+        $posts = static::all();
+
+        dd($posts->firstwhere('slug', $slug));
+    }
+}
+
+//$files = File::files(resource_path("posts/"));
+//
+//return array_map(fn($file) => $file->getContents(), $files);
+
+// if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+//     throw new ModelNotFoundException();
+//}
+//
+//return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,70 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//    $files = File::files(resource_path("posts"));
+//    $documents = [];
+//    foreach ($files as $file) {
+//        $documents = YamlFrontMatter::parseFile($file);
+//    }
+//  ddd($documents);
+//    $document = YamlFrontMatter::parseFile(
+//        resource_path('posts/my-fourth-post.html')
+//    );
+//
+//    ddd($document->date);
+//    return view('posts', [
+//        'posts' => Post::all()
+
+
+
+//    ]);
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
+
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z\-]+');
+
+
+//    $posts = array_map(function ($file) {
+//        $document = YamlFrontMatter::parseFile($file);
+//
+//        return new Post(
+//            $document->title,
+//            $document->excerpt,
+//            $document->date,
+//            $document->body(),
+//            $document->slug
+//        );
+//    }, $files);
+//
+
+
+//    $posts = array_map(function ($file) {
+//        $document = YamlFrontMatter::parseFile($file);
+//        return new Post(
+//            $document->title,
+//            $document->excerpt,
+//            $document->date,
+//            $document->body(),
+//            $document->slug
+//        );
+//    });
+
+//    ddd($posts[0]->title);
+
+//collect(File::files(resource_path("posts")))
+//    ->map(fn($file) => YamlFrontMatter::parseFile($file))
+//    ->map(fn($document) => new Post(
+//        $document->title,
+//        $document->excerpt,
+//        $document->date,
+//        $document->body(),
+//        $document->slug
+//    ));
